a23896f06bca1d8f148caa79047d9455bb537257
priyanshu Kumawat <priyanshu2004kumawat@gmail.com>
2026-06-12 15:07:59 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const app = express() ;
 const port = process.env.PORT || 5000 
 
 app.use(cors({
-    origin:"http://localhost:5173" , //request comes from frontend at this server Url
+    origin:"https://jarvis-2n0j.onrender.com" , //request comes from frontend at this server Url
     credentials:true 
 }))
 app.use(express.json()) 
